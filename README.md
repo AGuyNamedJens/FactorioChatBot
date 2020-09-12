7db8a598ab222b24bed20fb189bc681bf8958554
AGuyNamedJens <jens.wesseling@gmail.com>
2020-09-12 14:09:17 +0200

    Initial commit, almost ready for first release

---
+++ README.md
@@ -0,0 +1,71 @@
+# Factorio Chat Bot
+Two-Directional chat bot connecting Discord and Factorio chats together, written in nodejs.
+
+This bot does **not**:
+- Disable Achievements unless specified.
+- Require mods to run.
+
+The guide below is made for factorio servers on Linux.
+
+# Requirements
+  1. A factorio server with RCON enabled
+  2. A factorio server with Logging enabled
+  3. Ability to host the bridge locally on the same machine as the game server.
+
+# Setting up RCON and Logging
+The Factorio server must have RCON and Logging enabled.
+Add these params to the factorio's run script.
+
+```--rcon-port <port>```	Port to use for RCON
+  
+```--rcon-password <pass>```	Password for RCON
+
+```--console-log <path-to-file>```  File to log the chat messages in
+
+Full Example (including server params):
+
+```/opt/factorio/bin/64/factorio --start-server /opt/factorio/saves/map.zip --server-settings /opt/factorio/data/server-settings.json --rcon-port 8080 --rcon-password password --console-log /opt/factorio/Factorio-server.log```
+
+# Dependencies
+Install or update NodeJS https://nodejs.org/en/ (Version 12 or higher)
+
+To verify node is installed open an elevated cmd prompt and do "node -v" 
+the node version should be returned
+
+Navigate in the cmd prompt to the folder you have the Factorio Chat Bot at.
+
+Run the following commands within that directory
+
+`npm install discord.js --save`
+
+`npm install chokidar --save`
+
+`npm isntall rcon --save`
+
+# Configuration
+    
+  Rename `config.json.example` to `config.json`
+
+  Set `"logFile": "file"` to the path you set earlier. 
+
+  set `"chatChannel": "Channel ID"` with your discord channel ID for the chat messages. ([Help](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-))
+
+  Set `"cleanMessages": "false"` to true if you want to have clean messages in Factorio. **WARNING: THIS WILL DISABLE ACHIEVEMENTS!**
+
+  Set `"consoleChannel"` to the channel ID if you want to have a seperate channel for executing factorio commands. **WARNING: SOME MAY DISABLE ACHIEVEMENTS!**
+
+  set RconIP (Should remain 127.0.0.1 for localhost)
+
+  set RconPort (Same port you set in your server launch params)
+
+  set RconPassword (Same pass you set in your server launch params)
+
+
+# Running the bot
+--To run from console do the following in the dir of the bot-- 
+
+node FactorioChatBot.js
+
+--To run as a service--
+
+Follow the guide to install and set up [PM2](https://pm2.keymetrics.io/docs/usage/quick-start/)
---
+++ index.js
@@ -0,0 +1,134 @@
+//0.01 
+const Discord = require("discord.js");
+var fs = require('fs');
+var chokidar = require('chokidar');
+var Rcon = require('rcon');
+
+var config = require('./config.json');
+
+const bot = new Discord.Client();
+
+
+fs.writeFile(config.logFile, '', function(){
+	console.log('cleared previous chat log')
+});
+
+bot.login(config.token);
+
+var conn;
+//connect to Rcon
+function RconConnect()
+{
+	conn = new Rcon(config.RconIP, config.RconPort, config.RconPassword);
+
+		conn.on('auth', function() {
+		  console.log("Authenticated!");
+		  
+		  if(config.startupMessage.enabled) {
+			if(config.cleanMessages == true) {
+				conn.send('/silent-command game.print("[Chat System]: ' + config.startupMessage.message + '")');
+			} else {
+				conn.send('[Chat System]: ' + config.startupMessage.message);
+			}
+		  }
+
+		}).on('end', function() {
+		  console.log("Socket closed! Retrying to connect..");
+		  //failed to connect try again
+		  RconConnect();
+		}).on('error', function() {
+		  console.log("An error occoured. Retrying to connect..");
+		  //failed to connect try again
+		  RconConnect();
+		});
+		console.log("Connecting to the Factorio Server..");
+		conn.connect();
+}
+	
+ 
+//on bot start
+bot.on("ready", () => {
+
+	//connect to rcon
+	RconConnect();
+
+    console.log('Connected! Logged in as: '+ bot.user.username + ' - (' + bot.user.id + ')');
+    bot.channels.cache.get(config.chatChannel).send("[Chat System]: Online!")
+
+	//watch the log file for update
+	chokidar.watch(config.logFile, {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+		readLastLine(config.logFile);
+	});
+});
+
+//when we get a message from the chat channel on discord
+bot.on("message", (message) => {
+	if(message.content.length > 0 && !message.author.bot && message.channel.id === config.chatChannel)
+	{
+
+		// send to the server
+		
+		if(config.cleanMessages == true) {
+			conn.send('/silent-command game.print("[Discord] ' + message.author.username + ': ' + message.content + '")');
+		} else {
+			conn.send('[Discord] ' + message.author.username + ': ' + message.content);
+		}
+		// send to the channel showing someone sent a message to the server
+		message.channel.send(":speech_balloon: | `"+ message.author.username+ "`: " + message.content )
+
+		// delete their message
+        message.delete();
+	}
+});
+
+
+//-----------------------------------------------
+//user functions 
+
+function parseMessage(msg)
+{
+    var index = msg.indexOf(']');
+    var indexName = msg.indexOf(': ');
+    var newMsg = "`" + msg.slice(index+2, indexName) + "`" + msg.slice(indexName);
+
+	if (undefined !== msg && msg.length && index > 1)
+	{
+        //console.log(msg.slice(1,index), msg.slice(index+1));
+
+        if(msg.slice(1,index).includes("LEAVE")) bot.channels.cache.get(config.chatChannel).send(":red_circle: | " + msg.slice(index+2))
+        else if(msg.slice(1,index).includes("JOIN")) bot.channels.cache.get(config.chatChannel).send(":green_circle: | " + msg.slice(index+2))
+        else if(msg.slice(1,index).includes("CHAT") && !msg.includes("<server>")) bot.channels.cache.get(config.chatChannel).send(":speech_left: | " + newMsg)
+        else if(!msg.includes("<server>") && config.consoleChannel) bot.channels.cache.get(config.consoleChannel).send("? | " + msg.slice(index+1))
+	}
+	
+}
+
+function readLastLine(path)
+{
+	fs.readFile(path, 'utf-8', function(err, data) 
+	{
+		//get last line of file. 
+		if (err) throw err;
+		var lines = data.trim().split('\n');
+		lastLine = lines.slice(-1)[0];
+		console.log(lastLine);
+		if(path == config.logFile && lastLine.length > 0)  //logFile
+		{
+			//pasrs name and message
+			parseMessage(lastLine);
+		}		
+	});
+		
+	
+}
+
+
+
+
+
+
+
+
+
+
+
