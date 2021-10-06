78b131d46f032a1282f3cc7f0ec3e90e3695a99d
AGuyNamedJens <jens.wesseling@gmail.com>
2021-10-06 14:05:08 +0200

    Code formatting

--- index.js
+++ index.js
@@ -14,17 +14,16 @@ bot.login(config.token);
 var rcon;
 var tries = 1;
 
-function RconConnect()
-{
-	rcon = new Rcon({host: config.RconIP, port: config.RconPort, password: config.RconPassword});
+function RconConnect() {
+	rcon = new Rcon({ host: config.RconIP, port: config.RconPort, password: config.RconPassword });
 
 	rcon.connect().catch(error => {
 		console.error(error);
 
 		// try again with a max of 10 tries in a cooldown of 5 seconds per try
-		if(tries <= 10) {
+		if (tries <= 10) {
 			console.log(`Reconnect attempt ${tries}/10..`)
-			setTimeout(function() {
+			setTimeout(function () {
 				RconConnect();
 				tries++;
 			}, 5000)
@@ -36,26 +35,26 @@ function RconConnect()
 		console.log(`Connected to the factorio server!`)
 	});
 	// Authenticated, which means the authentication is successful and the system is online
-    rcon.on("authenticated", () => {
+	rcon.on("authenticated", () => {
 		console.log(`Authenticated!`)
 
 		clearLogFile();
 
-		if(config.startupMessage.enabled) {
-			if(config.cleanMessages == true) {
+		if (config.startupMessage.enabled) {
+			if (config.cleanMessages == true) {
 				rcon.send('/silent-command game.print("[Chat System]: ' + config.startupMessage.message + '")');
 			} else {
 				rcon.send('[Chat System]: ' + config.startupMessage.message);
 			}
-		}	
+		}
 	});
 
 	// In case any errors occour, log them to console and terminate the connection
-    rcon.on("error", (err) => {
+	rcon.on("error", (err) => {
 		console.error(`Error: ${err}`);
 		rcon.end();
 	});
-	
+
 	// In case the connection was ended, terminated or whatever else happened. Log a message and reconnect
 	rcon.on("end", () => {
 		console.log(`Socket connection ended! Reconnecting..`)
@@ -71,11 +70,11 @@ bot.on("ready", () => {
 	//connect to rcon
 	RconConnect();
 
-    console.log('Connected to Discord! Logged in as: '+ bot.user.username + ' - (' + bot.user.id + ')');
+	console.log('Connected to Discord! Logged in as: ' + bot.user.username + ' - (' + bot.user.id + ')');
 	bot.channels.cache.get(config.chatChannel).send("[Chat System]: Online!")
-	
+
 	//watch the log file for updates
-	chokidar.watch(config.logFile, {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+	chokidar.watch(config.logFile, { ignored: /(^|[\/\\])\../ }).on('all', (event, path) => {
 		readLastLine(config.logFile);
 	});
 });
@@ -84,27 +83,27 @@ bot.on("ready", () => {
 * Discord message event
 */
 
-bot.on("message", async(message) => {
-	if(!message.content.length > 0) return;
-	if(message.author.bot) return;
+bot.on("message", async (message) => {
+	if (!message.content.length > 0) return;
+	if (message.author.bot) return;
 
-	if(message.channel.id === config.chatChannel) {
+	if (message.channel.id === config.chatChannel) {
 		// send to the server
-		if(config.cleanMessages == true) {
+		if (config.cleanMessages == true) {
 			rcon.send('/silent-command game.print("[color=#7289DA][Discord] ' + message.author.username + ': ' + message.content + '[/color]")');
 		} else {
-			rcon.send('[color=#7289DA][Discord] ' + message.author.username + ': ' + message.content+ '[/color]');
+			rcon.send('[color=#7289DA][Discord] ' + message.author.username + ': ' + message.content + '[/color]');
 		}
 		// send to the channel showing someone sent a message to the server and delete their message from the channel
-		message.channel.send(":speech_balloon: | `"+ message.author.username+ "`: " + message.content);
+		message.channel.send(":speech_balloon: | `" + message.author.username + "`: " + message.content);
 		message.delete();
-		
-	} else if(message.channel.id === config.consoleChannel) {
+
+	} else if (message.channel.id === config.consoleChannel) {
 		// send command to the server
-		rcon.send('/'+ message.content);
+		rcon.send('/' + message.content);
 		// send to the channel showing someone sent a command to the server
-		message.channel.send("COMMAND RAN | `"+ message.author.username+ "`: " + message.content);
-	} else if(message.content.startsWith(`${config.prefix}online`)) {
+		message.channel.send("COMMAND RAN | `" + message.author.username + "`: " + message.content);
+	} else if (message.content.startsWith(`${config.prefix}online`)) {
 		// Command with the prefix defined in config.js to show online players
 		const players = await getOnlinePlayers();
 		// Send the message to the Discord channel
@@ -135,31 +134,30 @@ async function getOnlinePlayers() {
 * Chat function to parse the messages
 */
 
-function parseMessage(msg)
-{
-    var index = msg.indexOf(']');
-    var indexName = msg.indexOf(': ');
-    var newMsg = "`" + msg.slice(index+2, indexName) + "`" + msg.slice(indexName);
+function parseMessage(msg) {
+	var index = msg.indexOf(']');
+	var indexName = msg.indexOf(': ');
+	var newMsg = "`" + msg.slice(index + 2, indexName) + "`" + msg.slice(indexName);
 
 	if (msg.length && index > 1) {
-		if(msg.slice(1,index).includes("LEAVE")) {
+		if (msg.slice(1, index).includes("LEAVE")) {
 			// Send leave message to the Discord channel
-			bot.channels.cache.get(config.chatChannel).send(":red_circle: | " + msg.slice(index+2))
+			bot.channels.cache.get(config.chatChannel).send(":red_circle: | " + msg.slice(index + 2))
 			// Send leave message to the server
-			if(config.cleanMessages == true) rcon.send('/silent-command game.print("[color=red]'+ msg.slice(index+2) + '[/color]")');
-			else rcon.send('[color=red]' + msg.slice(index+2) + '[/color]');
-		} else if(msg.slice(1,index).includes("JOIN")){
+			if (config.cleanMessages == true) rcon.send('/silent-command game.print("[color=red]' + msg.slice(index + 2) + '[/color]")');
+			else rcon.send('[color=red]' + msg.slice(index + 2) + '[/color]');
+		} else if (msg.slice(1, index).includes("JOIN")) {
 			// Send join message to the Discord channel
-			bot.channels.cache.get(config.chatChannel).send(":green_circle: | " + msg.slice(index+2))
+			bot.channels.cache.get(config.chatChannel).send(":green_circle: | " + msg.slice(index + 2))
 			// Send join message to the server
-			if(config.cleanMessages == true) rcon.send('/silent-command game.print("[color=green]'+ msg.slice(index+2) + '[/color]")');
-			else rcon.send('[color=green]' + msg.slice(index+2) + '[/color]');
-		} else if(msg.slice(1,index).includes("CHAT") && !msg.includes("<server>")) {
+			if (config.cleanMessages == true) rcon.send('/silent-command game.print("[color=green]' + msg.slice(index + 2) + '[/color]")');
+			else rcon.send('[color=green]' + msg.slice(index + 2) + '[/color]');
+		} else if (msg.slice(1, index).includes("CHAT") && !msg.includes("<server>")) {
 			// Send incoming chat from the server to the Discord channel
 			bot.channels.cache.get(config.chatChannel).send(":speech_left: | " + newMsg)
-		} else if(!msg.includes("<server>") && config.consoleChannel !== "false") {
+		} else if (!msg.includes("<server>") && config.consoleChannel !== "false") {
 			// Send incoming message from the server, which has no category or user to the Discord console channel
-			bot.channels.cache.get(config.consoleChannel).send("? | " + msg.slice(index+1))
+			bot.channels.cache.get(config.consoleChannel).send("? | " + msg.slice(index + 1))
 		}
 	}
 }
@@ -168,25 +166,23 @@ function parseMessage(msg)
 * Logging function
 */
 
-function readLastLine(path)
-{
-	fs.readFile(path, 'utf-8', function(err, data) 
-	{
+function readLastLine(path) {
+	fs.readFile(path, 'utf-8', function (err, data) {
 		//get last line of file. 
 		if (err) throw err;
 		var lines = data.trim().split('\n');
 		lastLine = lines.slice(-1)[0];
 
 		// I should really optimize or completely remove this line
-		if(config.logLines == true) console.log(lastLine);
+		if (config.logLines == true) console.log(lastLine);
 
-		if(path == config.logFile && lastLine.length > 0) {
+		if (path == config.logFile && lastLine.length > 0) {
 			// Parse name and message and send it
 			parseMessage(lastLine);
-		}		
+		}
 	});
-		
-	
+
+
 }
 
 // Clear the logFile to prevent massive disk usage
@@ -201,12 +197,12 @@ function clearLogFile() {
 *	Returns an array of 2, first being the command response, second being the error (if there is one, otherwise it's empty)
 */
 async function rconCommand(command) {
-    if (!command.startsWith("/")) command = `/${command}`;
-    try {
-        let resp = await rcon.send(command);
-        if (typeof resp == "string" && resp.length) return resp;
-        else throw new Error("No length");
-    } catch (error) {
-        throw Error(`RCON Error --- Details --- \nNAME: ${error.name} \nDESC: ${error.description}`);
-    }
+	if (!command.startsWith("/")) command = `/${command}`;
+	try {
+		let resp = await rcon.send(command);
+		if (typeof resp == "string" && resp.length) return resp;
+		else throw new Error("No length");
+	} catch (error) {
+		throw Error(`RCON Error --- Details --- \nNAME: ${error.name} \nDESC: ${error.description}`);
+	}
 }
