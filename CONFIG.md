a0e38c9e5e053e34a73261ecb638d53a2e4c14e3
Mikhail Mikhalchuk <mikhailmcraft@gmail.com>
2022-01-19 18:20:14 -0500

    cleanup

--- CONFIG.md
+++ CONFIG.md
@@ -1,24 +1,24 @@
 # Configuration
     
-  - Rename `config.json.example` to `config.json` by using `mv config.json.example config.json`
+  Rename `config.json.example` to `config.json` by using `mv config.json.example config.json`
   > Open this file in either the terminal (`nano config.json`, `vim config.json` or any other terminal editors) or via any SFTP file manager.
 
   - Set `"logFile": "file"` to the path you set earlier. 
 
-  - set `"prefix": "?"` to a prefix of your choice. This is used for discord commands.
-
-  - set `"chatChannel": "Channel ID"` with your discord channel ID for the chat messages. ([Help](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-))
+  - set `"chatChannel": "Channel ID"` with your Discord channel ID for the chat messages. ([Help](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-))
 
   - set `"cleanMessages": false` to true if you want to have clean messages in Factorio. **WARNING: THIS WILL DISABLE ACHIEVEMENTS!**
 
-  - set `"consoleChannel": false` to the channel ID if you want to have a seperate channel for executing factorio commands. This won't return any responses however.. **WARNING: SOME MAY DISABLE ACHIEVEMENTS SUCH AS USING /COMMAND!**
+  - set `"adminsCanRunCommands": false` to true if you want users with administrative permissions on the Discord server to be able to remotely run Factorio commands as the server via `/command`.
 
-  - set `"logLines": false` to true if you want to have every line in the log file to be logged to console, includes chat, warnings and joins/leaves **WARNING: LOG SIZE WILL INCREASE BY A LOT AFTER A WHILE (GBs)**
+  - set `"sendServerMessages": false` to true if you want the server to send messages to the chat channel.
 
-  set RconIP (Should remain 127.0.0.1 for localhost)
+  - set `"logLines": false` to true if you want to have every line in the log file to be logged to console, includes chat, warnings and joins/leaves. **WARNING: LOG SIZE WILL INCREASE BY A LOT AFTER A WHILE (GBs)**
 
-  set RconPort (Same port you set in your server launch params)
+  - set `RconIP` to the RCON IP (should normally remain localhost, or 127.0.0.1, unless connecting to a remote Factorio server).
 
-  set RconPassword (Same pass you set in your server launch params)
+  - set `RconPort` to the RCON port (should normally remain 8080, unless connecting to a remote Factorio server).
 
-  set `"token": "Bot Token"` to the bot's token as i mentioned before when creating a new application.
+  - set `RconPassword` to the RCON password as set with the --rcon-password launch flag.
+
+  - set `"token": "Bot Token"` to the bot's token as mentioned when creating a new application.
--- index.ts
+++ index.ts
@@ -18,66 +18,31 @@ const Commands: Discord.Collection<string, Command> = new Discord.Collection();
 bot.login(config.token);
 
 interface Config {
-	/**
-	 * The file that the server is set to log to, as defined with the `--console-log` flag or config option.
-	 */
 	logFile: string;
 
-	/**
-	 * The channel the bot is set to send and receive messages from.
-	 */
 	chatChannel: string;
 
-	/**
-	 * Whether or not to clean log file messages upon startup.
-	 */
 	cleanMessages: boolean;
 
-	/**
-	 * Whether or not members with administrator permissions on the Discord server may run commands in-game via `?command`.
-	 */
 	adminsCanRunCommands: boolean;
 
-	/**
-	 * Whether or not to send miscellaneous server messages to the chat channel.
-	 */
 	sendServerMessages: boolean;
 
-	/**
-	 * Whether or not to log all lines read by the bot from the log file to the console.
-	 */
 	logLines: boolean;
 
 	startupMessage: {
-		/**
-		 * Whether or not the startup message is enabled.
-		 */
+		
 		enabled: boolean;
 
-		/**
-		 * The contents of the startup message, to be sent to the Factorio server.
-		 */
 		message: string;
 	}
 
-	/**
-	 * The RCON IP. Most of the time, this should be set to the localhost, `127.0.0.1`, unless connecting to a remote Factorio server.
-	 */
 	RconIP: string;
 
-	/**
-	 * The RCON port, as defined with the `--rcon-port` flag or config option. Most of the time, this should be set to `8080`, unless connecting to a remote Factorio server.
-	 */
 	RconPort: number;
 
-	/**
-	 * The RCON password, as defined with the `--rcon-password` flag or config option.
-	 */
 	RconPassword: string;
 
-	/**
-	 * The bot's token.
-	 */
 	token: string;
 }
 
@@ -93,6 +58,9 @@ interface Command {
 var rcon: Rcon;
 var tries = 1;
 
+/**
+ * Connects to the server via RCON.
+ */
 function RconConnect() {
 	rcon = new Rcon({ host: config.RconIP, port: config.RconPort, password: config.RconPassword });
 
@@ -118,8 +86,6 @@ function RconConnect() {
 	rcon.on("authenticated", () => {
 		console.log('Authenticated!');
 
-		clearLogFile();
-
 		if (config.startupMessage.enabled) {
 			if (config.cleanMessages == true) {
 				rcon.send('/silent-command game.print("[Chat System]: ' + config.startupMessage.message + '")');
@@ -130,13 +96,13 @@ function RconConnect() {
 		}
 	});
 
-	// In case any errors occour, log them to console and terminate the connection
+	// In case any errors occur, log them to console and terminate the connection
 	rcon.on("error", (err) => {
 		console.error(`Error: ${err}`);
 		rcon.end();
 	});
 
-	// In case the connection was ended, terminated or whatever else happened. Log a message
+	// Log a message on connection end
 	rcon.on("end", () => {
 		console.log('Socket connection ended!');
 	});
@@ -153,10 +119,14 @@ bot.on("ready", () => {
 	console.log(`Connected to Discord! Logged in as: ${bot.user.username} - (${bot.user.id})`);
 	(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send("[Chat System]: Online!");
 
+	clearLogFile();
+
 	//watch the log file for updates
 	chokidar.watch(config.logFile, { ignored: /(^|[\/\\])\../ }).on('all', (event, path) => {
 		readLastLine(config.logFile);
 	});
+
+	console.log('Watching log file.');
 
 	Commands.set("online", {
 		data: new SlashCommandBuilder()
@@ -219,7 +189,6 @@ bot.on('interactionCreate', async (interaction) => {
 /*
 * Discord message event
 */
-
 bot.on("messageCreate", async (message) => {
 	if (message.content.length <= 0) return;
 	if (message.author.bot) return;
@@ -236,11 +205,13 @@ bot.on("messageCreate", async (message) => {
 	}
 });
 
-// Get online players
-
-async function getOnlinePlayers() {
+/**
+ * Gets all players currently online on the Factorio server.
+ * @returns A promise that will be resolved with the list of the names of the currently online players.
+ */
+async function getOnlinePlayers(): Promise<string[]> {
 	// Run the command "/players online"
-	var res = await rconCommand("/p o");
+	var res = await rcon.send("/p o");
 	// Turn result into an array, remove the first and last array element
 	var res2 = res.split("\n").slice(1, -1);
 	// create a new array
@@ -255,17 +226,17 @@ async function getOnlinePlayers() {
 	return onlinePlayers;
 }
 
-/*
-* Chat function to parse the messages
-*/
-
-function parseMessage(msg: string | string[]) {
+/**
+ * Parses messages from the server log.
+ * @param msg The message to parse.
+ */
+function parseMessage(msg: string) {
 	var index = msg.indexOf(']');
 	var indexName = msg.indexOf(': ');
 	var newMsg = "`" + msg.slice(index + 2, indexName) + "`" + msg.slice(indexName);
 
 	if (msg.length && index > 1) {
-		if (msg.slice(1, index).includes("LEAVE")) {
+		if (msg.includes('[LEAVE]')) {
 			// Send leave message to the Discord channel
 			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send(":red_circle: | " + msg.slice(index + 2));
 			// Send leave message to the server
@@ -276,7 +247,7 @@ function parseMessage(msg: string | string[]) {
 				rcon.send('[color=red]' + msg.slice(index + 2) + '[/color]');
 			}
 		}
-		else if (msg.slice(1, index).includes("JOIN")) {
+		else if (msg.includes('[JOIN]')) {
 			// Send join message to the Discord channel
 			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send(":green_circle: | " + msg.slice(index + 2))
 			// Send join message to the server
@@ -287,25 +258,29 @@ function parseMessage(msg: string | string[]) {
 				rcon.send('[color=green]' + msg.slice(index + 2) + '[/color]');
 			}
 		}
-		else if (msg.slice(1, index).includes("CHAT") && !msg.includes("<server>")) {
+		else if (msg.includes("[CHAT]") && !msg.includes("[CHAT] <server>")) {
 			// Send incoming chat from the server to the Discord channel
 			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send(":speech_left: | " + newMsg)
 		}
-		else if (!msg.includes("<server>") && config.sendServerMessages) {
+		else if (msg.includes("[WARNING]")) {
+			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send(":warning: | " + newMsg)
+		}
+		else if (!msg.includes("] <server>") && config.sendServerMessages) {
 			// Send incoming message from the server, which has no category or user to the Discord console channel
-			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send("? | " + msg.slice(index + 1))
+			(bot.channels.cache.get(config.chatChannel) as Discord.TextChannel).send("? | " + msg)
 		}
 	}
 }
 
-/*
-* Logging function
-*/
-
+/**
+ * Reads the last line of a logging file.
+ * @param path The absolute path to the file.
+ */
 function readLastLine(path: fs.PathOrFileDescriptor) {
 	fs.readFile(path, 'utf-8', function (err, data) {
 		//get last line of file. 
 		if (err) throw err;
+		console.log(data.trim().replace(/\n/g, ''));
 		var lines = data.trim().split('\n');
 		var lastLine = lines.slice(-1)[0];
 
@@ -319,29 +294,12 @@ function readLastLine(path: fs.PathOrFileDescriptor) {
 			parseMessage(lastLine);
 		}
 	});
-
-
 }
 
-// Clear the logFile to prevent massive disk usage
+/**
+ * Clears the log file.
+ */
 function clearLogFile() {
-	fs.writeFile(config.logFile, "", function () {
-		console.log('Cleared previous chat log.');
-	});
-}
-
-/*	rconCommand function
-*	Returns an array of 2, first being the command response, second being the error (if there is one, otherwise it's empty)
-*/
-async function rconCommand(command: string) {
-	if (!command.startsWith("/")) command = `/${command}`;
-	try {
-		let resp = await rcon.send(command);
-		if (typeof resp == "string" && resp.length) {
-			return resp;
-		}
-	}
-	catch (error: any) {
-		throw new Error(`RCON Error \n--- Details --- \nNAME: ${error.name} \nDESC: ${error.description}`);
-	}
+	fs.writeFileSync(config.logFile, "");
+	console.log('Cleared previous chat log.');
 }
